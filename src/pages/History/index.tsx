ba4dc7360641ddcf5711925c94bf2739f7161c32
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-08-02 21:18:52 -0300

    Componente: Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -20,25 +20,33 @@ export function History() {
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>30 Minutos</td>
               <td>Há cerca de 9 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="red">Cancelado</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>10 Minutos</td>
               <td>Há cerca de 1 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há cerca de 4 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
